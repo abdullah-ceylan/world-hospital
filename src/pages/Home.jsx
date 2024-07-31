12417f0eb87acabf1e18d623a830225f8fd9c9eb
axel-ac <abdullahceylande@gmail.com>
2024-07-31 08:54:41 +0200

    navbar-scroll

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import Image from "react-bootstrap/Image";
 import Img from "../helpers/Hospital.jpg";
 import Doctors from "../components/Doctors";
+import NavScroll from "../components/NavScroll";
+
 
 const Home = () => {
   return (
     <>
+    <NavScroll/>
       <div>
         <Image src={Img} fluid />
       </div>
